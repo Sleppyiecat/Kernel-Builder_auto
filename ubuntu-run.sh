5dd81c81e12f8412558ce3096a6ed9c8634c99fc
Gabriel <adeilmazk@gmail.com>
2020-12-18 19:29:20 -0300

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -4,18 +4,27 @@
             sudo -E apt-get -qq install bc build-essential zip libstdc++6 git wget gcc clang libssl-dev repo rsync flex curl ccache bison aria2
             git clone https://github.com/Gabriel260/android_kernel_samsung_exynos7885 -b upstream kernel
             cd kernel
-            git clone https://github.com/Gabriel260/Linaro-V6.5.0 toolchain
+            git clone https://github.com/Chatur27/Toolchains-for-Eureka -b proton_clang_12 toolchain
             export ARCH=arm64
             export ANDROID_MAJOR_VERSION=q
             export KBUILD_BUILD_USER=Gabriel
             export KBUILD_BUILD_HOST=Ubuntu
             export CROSS_COMPILE=$(pwd)/toolchain/bin/aarch64-linux-gnu-
             export CROSS_COMPILE_ARM32=$(pwd)/toolchain/bin/arm-linux-gnueabi-
-            make clean
-            make mrproper
-            make a10_defconfig
-            make -j4
-            cd arch/arm64/boot
+	
+            export LOCALVERSION=-devel
+	    make O=out ARCH=arm64 ANDROID_MAJOR_VERSION=q a10_defconfig
+	    PATH="$(pwd)/toolchain/bin:$(pwd)/toolchain/bin:${PATH}" \
+	    make -j4 O=out \
+	    ARCH=arm64 \
+	    ANDROID_MAJOR_VERSION=q \
+	    CC=clang \
+	    LD_LIBRARY_PATH="$(pwd)/toolchain/lib:$LD_LIBRARY_PATH" \
+	    CLANG_TRIPLE=aarch64-linux-gnu- \
+	    CROSS_COMPILE=aarch64-linux-gnu- \
+	    CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+            cd out/arch/arm64/boot
             curl -sL https://git.io/file-transfer | sh
             ./transfer wet Image
             ./transfer wet dtbo.img
