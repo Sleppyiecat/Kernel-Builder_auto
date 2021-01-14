3e90a63e9fbf5df205098cdb3d75690e8b8acf36
Karou <71267804+Sleppyiecat@users.noreply.github.com>
2021-01-14 16:55:09 +0100

    ubuntu-run.sh: get rid of unused clone script

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -2,7 +2,6 @@
             git config --global user.email "mruczek70@gmail.com"         
             sudo -E apt-get -qq update
             sudo -E apt-get -qq install bc build-essential zip libstdc++6 git wget gcc clang libssl-dev repo rsync flex curl ccache bison aria2
-            git clone https://github.com/Sleppyiecat/android_kernel_samsung_exynos7885
             cd android_kernel_samsung_exynos7885
             git clone https://github.com/kdrag0n/proton-clang --depth=1 -b master toolchain
 	    
