99d5d6388429a7ac29fb6cde4f374b60384d49bb
Karou <71267804+Sleppyiecat@users.noreply.github.com>
2021-03-09 23:23:29 +0100

    Well lets try

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -2,13 +2,12 @@
             git config --global user.email "mruczek70@gmail.com"         
             sudo -E apt-get -qq update
             sudo -E apt-get -qq install bc build-essential zip libstdc++6 git wget gcc clang libssl-dev repo rsync flex curl ccache bison aria2
-            cd android_kernel_samsung_exynos7885
+            cd davinci
             git clone https://github.com/kdrag0n/proton-clang --depth=1 -b master toolchain
 	    
 	    echo "_________________________________________________"
-	    echo "=               projectNULL_kernel              ="
-	    echo "=              Main Developer: Yuuko            ="
-	    echo "=             CO-Developer: chrissunil          ="
+	    echo "=             VantomBE for davinci              ="
+	    echo "=                Developer: Karou               ="
 	    echo "=                                               ="
 	    echo "=     Compiling kernel using Proton Clang 12    ="
 	    echo "=---------------------------------------------- ="
@@ -17,13 +16,13 @@
 	    echo -e "=_______________________________________________="
 	    
             export ARCH=arm64
-            export ANDROID_MAJOR_VERSION=q
-            export KBUILD_BUILD_USER=Yuuko
+            export ANDROID_MAJOR_VERSION=r
+            export KBUILD_BUILD_USER=Karou
             export KBUILD_BUILD_HOST=Ubuntu
             export CROSS_COMPILE=$(pwd)/toolchain/bin/aarch64-linux-gnu-
             export CROSS_COMPILE_ARM32=$(pwd)/toolchain/bin/arm-linux-gnueabi-
 	
-	    make O=out ARCH=arm64 ANDROID_MAJOR_VERSION=q a10_new_defconfig
+	    make O=out ARCH=arm64 ANDROID_MAJOR_VERSION=r davinci_defconfig
 	    PATH="$(pwd)/toolchain/bin:$(pwd)/toolchain/bin:${PATH}" \
 	    make -j4 O=out \
 	    ARCH=arm64 \
@@ -36,7 +35,7 @@
 
             cd out/arch/arm64/boot
             curl -sL https://git.io/file-transfer | sh
-            ./transfer wet Image
+            ./transfer wet Image.gz-dtb
             ./transfer wet dtbo.img
 	    
 	    echo "_________________________________________________"
