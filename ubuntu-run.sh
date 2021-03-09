31af18334ede4ac8b39d37461dbf46d8bf02f72f
Karou <71267804+Sleppyiecat@users.noreply.github.com>
2021-03-10 00:20:54 +0100

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -2,23 +2,23 @@
             git config --global user.email "mruczek70@gmail.com"         
             sudo -E apt-get -qq update
             sudo -E apt-get -qq install bc build-essential zip libstdc++6 git wget gcc clang libssl-dev repo rsync flex curl ccache bison aria2
-            cd ..
+            cd davinci
             mkdir out
             KERNEL_ZIP_NAME=${VERSION}_kernel_$(date +%F).zip
  	    if [ -d "out/arch/arm/boot/dts" ]; then
 		   rm out/arch/arm/boot/dts/*;
 	    fi;
             export ARCH=arm
-            export KBUILD_BUILD_USER=Yuuko
+            export KBUILD_BUILD_USER=Karou
             export KBUILD_BUILD_HOST=Ubuntu
             export LOCALVERSION=-${VERSION}
-            export CROSS_COMPILE=$(pwd)/Toolchains-for-Eureka/bin/arm-eabi-
+            export CROSS_COMPILE=$(pwd)/Toolchains/bin/arm-eabi-
             make O=out a10_new_defconfig
             make O=out -j4
             make O=out dtbs
 	    ./scripts/mkdtimg.sh -i $(pwd)/arch/arm/boot/dts/ -o dt.img;
-            find $(pwd) -name "Image" -exec mv -f {} $(pwd)/kernel_zip/tools \;
-	    find $(pwd) -name "dt.img" -exec mv -f {} $(pwd)/kernel_zip/tools \;
+            find $(pwd) -name "Image.gz-dtb" -exec mv -f {} $(pwd)/kernel_zip/tools \;
+	    find $(pwd) -name "dtbo.img" -exec mv -f {} $(pwd)/kernel_zip/tools \;
 	    cp -f out/drivers/net/wireless/sc2331/sprdwl.ko $(pwd)/kernel_zip/tools;
 	    cd $(pwd)/kernel_zip;
 	    zip -r ${KERNEL_ZIP_NAME} ./;
