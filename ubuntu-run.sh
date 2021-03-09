3c3f0279804d04b12cda8954c28335f1ac0b47f3
Karou <71267804+Sleppyiecat@users.noreply.github.com>
2021-03-10 00:24:30 +0100

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -4,27 +4,12 @@
             sudo -E apt-get -qq install bc build-essential zip libstdc++6 git wget gcc clang libssl-dev repo rsync flex curl ccache bison aria2
             cd davinci
             mkdir out
-            KERNEL_ZIP_NAME=${VERSION}_kernel_$(date +%F).zip
- 	    if [ -d "out/arch/arm/boot/dts" ]; then
-		   rm out/arch/arm/boot/dts/*;
-	    fi;
             export ARCH=arm
             export KBUILD_BUILD_USER=Karou
             export KBUILD_BUILD_HOST=Ubuntu
             export LOCALVERSION=-${VERSION}
             export CROSS_COMPILE=$(pwd)/Toolchains/bin/arm-eabi-
-            make O=out a10_new_defconfig
+            make O=out davinci_defconfig
             make O=out -j4
-            make O=out dtbs
-	    ./scripts/mkdtimg.sh -i $(pwd)/arch/arm/boot/dts/ -o dt.img;
-            find $(pwd) -name "Image.gz-dtb" -exec mv -f {} $(pwd)/kernel_zip/tools \;
-	    find $(pwd) -name "dtbo.img" -exec mv -f {} $(pwd)/kernel_zip/tools \;
-	    cp -f out/drivers/net/wireless/sc2331/sprdwl.ko $(pwd)/kernel_zip/tools;
-	    cd $(pwd)/kernel_zip;
-	    zip -r ${KERNEL_ZIP_NAME} ./;
-	    mv *.zip ..
-            cd ..
-            curl -sL https://git.io/file-transfer | sh
-            ./transfer wet *.zip
-            df -h
+            make O=out 
             sleep 60
